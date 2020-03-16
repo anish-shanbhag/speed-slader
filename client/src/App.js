5b051d112ced786d86384a4cffd978ad760fe9e0
Anish Shanbhag <anish.shanbhag.development@gmail.com>
2020-03-16 14:23:50 -0700

    Add Slader query functionality to client/server

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import HashLoader from "react-spinners/HashLoader";
 import _ from "lodash";
 import axios from "axios";
 
 export default class App extends React.Component {
   constructor () {
     super();
-    this.throttleAssignmentChange = _.throttle(this.assignmentChange, 1000);
+    this.throttleAssignmentChange = _.debounce(this.assignmentChange, 1000);
+    this.state = {
+      solutions: [],
+      range: [],
+      page: null,
+      id: 0,
+      loading: false,
+      error: false
+    }
   }
   render () {
     return (
@@ -22,48 +31,127 @@ export default class App extends React.Component {
             For example: 646 21
           </div>
           <input type="text" onChange={e => this.throttleAssignmentChange(e.target.value)}></input>
+          <div>
+            You can also enter a smaller range of problems in the text box below to view just those problems
+            For example, entering "489 1-23 odd" in the assignment text box and then just "4-5" in the text box below would only show problems 4 and 5 from that assignment.
+          </div>
+          <input type="text" onChange={e => this.changeRange(e.target.value)}></input>
+          <div>
+            {this.state.error && "An error occurred. Make sure you entered the assignment correctly."}
+            <HashLoader
+              size={100}
+              color={"#ff8800"}
+              loading={this.state.loading}
+            />
+            {this.state.rangeError ? "Invalid problem range." : this.state.range && this.state.solutions.filter(solution => this.state.range.length === 0 || this.state.range.includes(solution.problem)).map(solution =>
+              <>
+              <div className="solution-container">
+                <div className="solution-problem">{solution.letter ? solution.problem + solution.letter : solution.problem}.</div>
+                <img src={'data:image/png;base64,' + solution.image.data} width="95%"></img>
+              </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
     );
   }
   async assignmentChange (assignment) {
+    const oldID = this.state.id;
+    this.setState(state => ({
+      loading: true,
+      error: false,
+      id: state.id + 1
+    }));
+    try {
+      const page = assignment.substring(0, assignment.indexOf(" "));
+      let problems = this.getRange(assignment.substring(assignment.indexOf(" ")));
+      const getFilteredSolutions = state => state.page === page ? state.solutions.filter(solution => problems.includes(solution.problem)) : [];
+      this.setState(state => ({
+        solutions: getFilteredSolutions(state),
+        page,
+        promises: []
+      }));
+      problems = problems.filter(problem => !getFilteredSolutions(this.state).map(solution => solution.problem).includes(problem)).map(async problem => {
+        return (new Promise(async (resolve, reject) => {
+          try {
+            await this.addSolution(this.state.id, page, problem);
+            resolve();
+          } catch (e) {
+            let letters = 0;
+            for (let letter = "a";; letter = String.fromCharCode(letter.charCodeAt() + 1)) {
+              try {
+                await this.addSolution(this.state.id, page, problem, letter);
+              } catch {
+                if (letters === 0) reject();
+                else break;
+              } finally {
+                letters++;
+              }
+            }
+            resolve();
+          }
+        })).catch(e => {
+          this.setState({
+            error: true
+          });
+        });
+      });
+      await Promise.all(problems);
+      if (this.state.id === oldID + 1) {
+        this.setState({
+          loading: false
+        });
+      }
+    } catch (e) {
+      console.error(e);
+      this.setState({
+        error: true
+      });
+    }
+  }
+  async addSolution (id, page, problem, letter) {
+    const image = {
+      problem,
+      letter,
+      image: await axios({
+        method: "GET",
+        url: "http://localhost:4000",
+        params: {
+          page,
+          problem: letter ? problem + letter : problem
+        }
+      })
+    }
+    if (this.state.id === id) {
+      this.setState(state => ({
+        solutions: state.solutions.concat([image]).sort((a, b) => a.problem > b.problem ? 1 : a.problem < b.problem ? -1 : a.letter > b.letter ? 1 : -1)
+      }));
+    }
+  }
+  getRange (rangeText) {
     const range = (start, end) => Array(end - start + 1).fill().map((_, idx) => start + idx);
-    const problems = assignment.replace(/\s/g, "").split(",").map(set => {
+    return rangeText.replace(/\s/g, "").split(",").map(set => {
       const odd = set.includes("odd");
       const [start, end] = set.replace("odd", "").split("-").map(number => parseInt(number));
       return range(start, end ? end : start).filter(problem => !odd || problem % 2 === 1);
     }).flat().sort((a, b) => a - b);
-
-    problems.forEach(async problem => {
-      (await new Promise(async (resolve, reject) => {
-        try {
-          resolve([await axios({
-            method: "GET",
-            url: "http://localhost:4000",
-            params: {
-              problem
-            }
-          })]);
-        } catch {
-          const images = [];
-          try {
-            for (let letter = "a";; letter = String.fromCharCode(letter.charCodeAt() + 1)) {
-              images.push(await axios({
-                method: "GET",
-                url: "http://localhost:4000",
-                params: {
-                  problem: problem + letter
-                }
-              }))
-            }
-          } catch {
-            if (images.length === 0) reject(new Error("Invalid problem!"));
-            else resolve(images);
-          }
-        }
-      })).forEach(images => {
-        console.log(images);
-      })
+  }
+  changeRange (rangeText) {
+    this.setState({
+      rangeError: false
+    }, () => {
+      let range;
+      try {
+        range = rangeText.length === 0 ? [] : this.getRange(rangeText);
+        this.setState({
+          range
+        });
+      } catch {
+        this.setState({
+          rangeError: true
+        });
+      }
     });
   }
 }
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const puppeteer = require("puppeteer");
+const pLimit = require("p-limit");
 
 const app = express();
 const port = 4000;
@@ -10,9 +11,59 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  setTimeout(() => res.send(req.query), Math.random() * 2);
-  //res.send(req)
+const limit = pLimit(3);
+let requests = 0;
+let start = Date.now();
+
+app.get("/", (req, res, next) => {
+  limit(async () => {
+    requests++;
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1920,
+      height: 1080
+    });
+    try {
+      let navigated = false;
+      while (!navigated) {
+        try {
+          await page.goto(`https://www.slader.com/textbook/9781285740621-stewart-calculus-8th-edition/${req.query.page}/exercises/${req.query.problem}/`, {
+            timeout: 10000
+          });
+          navigated = true;
+        } catch {
+          continue;
+        }
+      }
+      await page.waitFor(".contents, .return-home-button");
+      const solution = await page.$(".contents");
+      if (!solution) {
+        next();
+        return;
+      }
+      let screenshotted = false;
+      let image;
+      while (!screenshotted) {
+        try {
+          image = await solution.screenshot({
+            encoding: "base64"
+          });
+          screenshotted = true;
+        } catch {
+          continue;
+        }
+      }
+      res.send(image);
+    } catch (e) {
+      next(e);
+    } finally {
+      console.log((Date.now() - start) / requests);
+      await browser.close();
+    }
+  });
 });
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}.`))
