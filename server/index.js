ba6fb05508c0ded70e0315f3aede63b1651a8055
Anish Shanbhag <anish.shanbhag.development@gmail.com>
2020-03-16 17:45:30 -0700

    Add header Access-Control-Allow-Methods to server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const port = process.env.dev ? 4000 : process.env.PORT;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
