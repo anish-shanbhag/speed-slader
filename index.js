8f0e3b17fbe2e2a4cda40ae38d11e6e277c15cca
Anish Shanbhag <anish.shanbhag.development@gmail.com>
2020-03-16 18:33:02 -0700

    Restructured project to enable auto deployment

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ const pLimit = require("p-limit");
 const app = express();
 const port = process.env.dev ? 4000 : process.env.PORT;
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
